8be5b08679a3a23b5307b6d180a326c235277fa1
Tristen Horton <tristen@tristenhorton.com>
2017-06-09 19:17:39 -0700

    ComboBoxItem is used as an item value for the Split combo box
    Using an object makes keeping track of the object thats referenced more object oriented and less tedious (such as using a string and a dictionary to match the split info, or a string and an enum type)

---
+++ ComboBoxItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplit.HollowKnight {
+	public class ComboBoxItem {
+
+		public string Text { get; private set; }
+		public SplitInfo Tag { get; private set; }
+
+		public ComboBoxItem(string text, SplitInfo tag) {
+			Text = text;
+			Tag = tag;
+		}
+
+		public override string ToString() {
+			return Text;
+		}
+	}
+}
