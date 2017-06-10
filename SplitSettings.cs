d110b7f0a59cfe66ddf654e6a90d75ddcdea90a8
Tristen Horton <tristen@tristenhorton.com>
2017-06-09 17:57:28 -0700

    Split Info Integration
    Since SplitInfo is an object that contains all of the information regarding a split's name, description, and tooltip - we no longer need helper functions to get information from attributes.

---
+++ SplitSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LiveSplit.HollowKnight {
+	public partial class SplitSettings : UserControl {
+
+		public SplitInfo Split { get; private set; }
+
+		public SplitSettings() {
+			InitializeComponent();
+		}
+
+		public void SetSplit(SplitInfo split) {
+			foreach (var item in cboName.Items) {
+				if ((item as ComboBoxItem).Tag.Equals(split)) {
+					Split = split;
+					cboName.SelectedItem = item;
+				}
+			}
+		}
+
+		private void cboName_SelectedIndexChanged(object sender, EventArgs e) {
+			var splitDescription = (cboName.SelectedItem as ComboBoxItem).Text;
+			Split = (cboName.SelectedItem as ComboBoxItem).Tag as SplitInfo;
+			
+			ToolTips.SetToolTip(cboName, Split.ToolTip);
+		}
+	}
+}
