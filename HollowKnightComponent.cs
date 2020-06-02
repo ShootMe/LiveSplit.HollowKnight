960ef91c2a160510acabc6c4f959eb44b30894be
5FiftySix6 <ybham6@gmail.com>
2020-06-02 12:59:21 -0400

    Don't have timer running during load for same-room dupe.

--- HollowKnightComponent.cs
+++ HollowKnightComponent.cs
@@ -125,7 +125,7 @@ namespace LiveSplit.HollowKnight {
                         && (uiState != UIState.PAUSED || loadingMenu)
                         && (!string.IsNullOrEmpty(nextScene) || sceneName == "_test_charms" || loadingMenu)
                         && nextScene != sceneName)
-                    || (nextScene != sceneName && mem.TileMapDirty() && !mem.UsesSceneTransitionRoutine());
+                    || (mem.TileMapDirty() && !mem.UsesSceneTransitionRoutine());
 
                 lastGameState = gameState;
             }
