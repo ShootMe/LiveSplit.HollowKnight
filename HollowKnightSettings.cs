03ac28e931f634b8eec0072feae2ce995a5b6554
slaurent <slaurent22@pm.me>
2021-09-19 15:40:24 -0400

    improve settings ui
    - do not preload the full list for each split's combobox.
    - add an edit button to toggle into the full selectible data source
    - cache available splits (both sortings) to avoid duplicating the work
    - mark editable on drag
    - add above/add below

--- HollowKnightSettings.cs
+++ HollowKnightSettings.cs
@@ -11,6 +11,8 @@ namespace LiveSplit.HollowKnight {
         public bool Ordered { get; set; }
         public bool AutosplitEndRuns { get; set; }
         private bool isLoading;
+        private List<string> availableSplits = new List<string>();
+        private List<string> availableSplitsAlphaSorted = new List<string>();
 
         public HollowKnightSettings() {
             isLoading = true;
@@ -43,7 +45,8 @@ namespace LiveSplit.HollowKnight {
                 DescriptionAttribute description = (DescriptionAttribute)info.GetCustomAttributes(typeof(DescriptionAttribute), false)[0];
 
                 HollowKnightSplitSettings setting = new HollowKnightSplitSettings();
-                setting.cboName.DataSource = GetAvailableSplits();
+                setting.cboName.DataSource = new List<string>() { description.Description };
+                setting.cboName.Enabled = false;
                 setting.cboName.Text = description.Description;
                 AddHandlers(setting);
 
@@ -56,10 +59,16 @@ namespace LiveSplit.HollowKnight {
         private void AddHandlers(HollowKnightSplitSettings setting) {
             setting.cboName.SelectedIndexChanged += new EventHandler(ControlChanged);
             setting.btnRemove.Click += new EventHandler(btnRemove_Click);
+            setting.btnEdit.Click += new EventHandler(btnEdit_Click);
+            setting.btnAddAbove.Click += new EventHandler(btnAddAbove_Click);
+            setting.btnAddBelow.Click += new EventHandler(btnAddBelow_Click);
         }
         private void RemoveHandlers(HollowKnightSplitSettings setting) {
             setting.cboName.SelectedIndexChanged -= ControlChanged;
             setting.btnRemove.Click -= btnRemove_Click;
+            setting.btnEdit.Click -= btnEdit_Click;
+            setting.btnAddAbove.Click -= btnAddAbove_Click;
+            setting.btnAddBelow.Click -= btnAddBelow_Click;
         }
         public void btnRemove_Click(object sender, EventArgs e) {
             for (int i = flowMain.Controls.Count - 1; i > 0; i--) {
@@ -71,6 +80,48 @@ namespace LiveSplit.HollowKnight {
                 }
             }
             UpdateSplits();
+        }
+        public void btnEdit_Click(object sender, EventArgs e) {
+            for (int i = flowMain.Controls.Count - 1; i > 0; i--) {
+                if (flowMain.Controls[i].Contains((Control)sender)) {
+                    HollowKnightSplitSettings setting = (HollowKnightSplitSettings)((Button)sender).Parent;
+                    if (setting.cboName.Enabled) {
+                        disableEdit(setting);
+                    } else {
+                        enableEdit(setting);
+                    }
+                    break;
+                }
+            }
+        }
+        private void btnAddAbove_Click(object sender, EventArgs e) {
+            for (int i = flowMain.Controls.Count - 1; i > 0; i--) {
+                if (flowMain.Controls[i].Contains((Control)sender)) {
+                    HollowKnightSplitSettings setting = (HollowKnightSplitSettings)((Button)sender).Parent;
+                    int index = setting.Parent.Controls.GetChildIndex(setting);
+                    addSplitAtIndex(index);
+                }
+            }
+        }
+        private void btnAddBelow_Click(object sender, EventArgs e) {
+            for (int i = flowMain.Controls.Count - 1; i > 0; i--) {
+                if (flowMain.Controls[i].Contains((Control)sender)) {
+                    HollowKnightSplitSettings setting = (HollowKnightSplitSettings)((Button)sender).Parent;
+                    int index = setting.Parent.Controls.GetChildIndex(setting);
+                    addSplitAtIndex(index + 1);
+                }
+            }
+        }
+        private void enableEdit(HollowKnightSplitSettings setting) {
+            string currentText = setting.cboName.Text;
+            setting.btnEdit.Text = "✔";
+            setting.cboName.DataSource = GetAvailableSplits();
+            setting.cboName.Text = currentText;
+            setting.cboName.Enabled = true;
+        }
+        private void disableEdit(HollowKnightSplitSettings setting) {
+            setting.btnEdit.Text = "✏";
+            setting.cboName.Enabled = false;
         }
         public void ControlChanged(object sender, EventArgs e) {
             UpdateSplits();
@@ -138,37 +189,49 @@ namespace LiveSplit.HollowKnight {
                 Splits.Add(split);
             }
         }
-        private void btnAddSplit_Click(object sender, EventArgs e) {
+        private HollowKnightSplitSettings createSetting() {
             HollowKnightSplitSettings setting = new HollowKnightSplitSettings();
             List<string> splitNames = GetAvailableSplits();
             setting.cboName.DataSource = splitNames;
             setting.cboName.Text = splitNames[0];
+            setting.btnEdit.Text = "✔";
             AddHandlers(setting);
-
+            return setting;
+        }
+        private void btnAddSplit_Click(object sender, EventArgs e) {
+            HollowKnightSplitSettings setting = createSetting();
             flowMain.Controls.Add(setting);
             UpdateSplits();
         }
+        private void addSplitAtIndex(int index) {
+            HollowKnightSplitSettings setting = createSetting();
+            flowMain.Controls.Add(setting);
+            flowMain.Controls.SetChildIndex(setting, index);
+            UpdateSplits();
+        }
         private List<string> GetAvailableSplits() {
-            List<string> splits = new List<string>();
-            foreach (SplitName split in Enum.GetValues(typeof(SplitName))) {
-                MemberInfo info = typeof(SplitName).GetMember(split.ToString())[0];
-                DescriptionAttribute description = (DescriptionAttribute)info.GetCustomAttributes(typeof(DescriptionAttribute), false)[0];
-                splits.Add(description.Description);
-            }
-            if (rdAlpha.Checked) {
-                splits.Sort(delegate (string one, string two) {
+            if (availableSplits.Count == 0) {
+                foreach (SplitName split in Enum.GetValues(typeof(SplitName))) {
+                    MemberInfo info = typeof(SplitName).GetMember(split.ToString())[0];
+                    DescriptionAttribute description = (DescriptionAttribute)info.GetCustomAttributes(typeof(DescriptionAttribute), false)[0];
+                    availableSplits.Add(description.Description);
+                    availableSplitsAlphaSorted.Add(description.Description);
+                }
+                availableSplitsAlphaSorted.Sort(delegate (string one, string two) {
                     return one.CompareTo(two);
                 });
             }
-            return splits;
+            return rdAlpha.Checked ? availableSplitsAlphaSorted : availableSplits;
         }
         private void radio_CheckedChanged(object sender, EventArgs e) {
             foreach (Control c in flowMain.Controls) {
                 if (c is HollowKnightSplitSettings) {
                     HollowKnightSplitSettings setting = (HollowKnightSplitSettings)c;
-                    string text = setting.cboName.Text;
-                    setting.cboName.DataSource = GetAvailableSplits();
-                    setting.cboName.Text = text;
+                    if (setting.cboName.Enabled) {
+                        string text = setting.cboName.Text;
+                        setting.cboName.DataSource = GetAvailableSplits();
+                        setting.cboName.Text = text;
+                    }
                 }
             }
         }
@@ -190,6 +253,7 @@ namespace LiveSplit.HollowKnight {
                 e.Effect = DragDropEffects.Move;
                 int oldIndex = destination.Controls.GetChildIndex(data);
                 if (oldIndex != index) {
+                    enableEdit(data);
                     destination.Controls.SetChildIndex(data, index);
                     destination.Invalidate();
                 }
