747e0d45438c2f7a83704f7a830107b34d2da531
cerpintext <cerpintext@protonmail.com>
2022-01-15 14:36:18 +0100

    fixed the trans start/end issue

--- HollowKnightStoredData.cs
+++ HollowKnightStoredData.cs
@@ -80,7 +80,9 @@ namespace LiveSplit.HollowKnight
                 pdInts[offset] = mem.PlayerData<int>(offset);
             }
             
-            if (mem.HeroTransitionState() != HeroTransitionState.WAITING_TO_TRANSITION || mem.GameState() is GameState.EXITING_LEVEL or GameState.LOADING) {
+            if (mem.HeroTransitionState() != HeroTransitionState.WAITING_TO_TRANSITION 
+                || mem.GameState() is GameState.EXITING_LEVEL or GameState.LOADING
+                || mem.SceneName() != mem.NextSceneName()) {
                 // In transition
                 TraitorLordDeadOnEntry = mem.PlayerData<bool>(Offset.killedTraitorLord);
             } else {
